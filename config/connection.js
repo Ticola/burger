73640ceab62459a78826b035e15d8671319ab185
Edgar Reynaga <edgar.reynaga@loop.colum.edu>
2018-12-13 15:20:50 -0600

    ER

---
+++ config/connection.js
@@ -0,0 +1,19 @@
+var mysql = require('mysql');
+var connection;
+
+if (process.env.JAWSDB_URL) {
+    connection = mysql.createConnection(process.env.JAWSDB_URL);
+}
+else {
+    connection = mysql.createConnection({
+        host: "127.0.0.1",
+        port: 8889,
+        user: `root`,
+        password: `root`,
+        database: `burgers_db`
+    });
+}
+
+
+connection.connect();
+module.exports = connection;
---
+++ db/seeds.sql
@@ -0,0 +1,10 @@
+INSERT INTO burgers
+    (burger_name, devoured)
+VALUES
+    ("Big Mac", false),
+    ("Whooper", false),
+    ("Cheeseburger", false),
+    ("Baconator", false),
+    ("Quarter Pounder", false),
+    ("McRoyal", false),
+    ("Double Double", true);
