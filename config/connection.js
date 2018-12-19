c19ab6373e2486525cb7d63f0b623ed9badef4f6
Edgar Reynaga <edgar.reynaga@loop.colum.edu>
2018-12-18 21:46:41 -0600

    eR

--- config/connection.js
+++ config/connection.js
@@ -6,11 +6,11 @@ if (process.env.JAWSDB_URL) {
 }
 else {
     connection = mysql.createConnection({
-        host: "127.0.0.1",
-        port: 8889,
-        user: `root`,
-        password: `root`,
-        database: `burgers_db`
+        host: "qzkp8ry756433yd4.cbetxkdyhwsb.us-east-1.rds.amazonaws.com",
+        port: 3306,
+        user: `jei0cam4u57b22c8`,
+        password: `v81ypl0eepqpnuuh`,
+        database: `guwf7enz212ivz1p`
     });
 }
 
