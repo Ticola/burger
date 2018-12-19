c9517899c7b87b0c133d7c6e0707ca18ec861d71
Edgar Reynaga <edgar.reynaga@loop.colum.edu>
2018-12-18 21:40:03 -0600

    ER

--- controllers/burgers_controller.js
+++ controllers/burgers_controller.js
@@ -15,7 +15,7 @@ router.get('/index', function (req, res) {
 });
 
 router.post('/burger/create', function (req, res) {
-    burger.insertOne(req.body.burger_name, function () {
+    burger.insertOne(["burger_name", "devoured"], [req.body.burger_name,], function () {
         res.redirect('/index');
     });
 });
